ce1f960c1d5c7d529cbe0251a9ed39423945c4a2
jqn <jan.horvat@gmail.com>
2015-01-07 16:51:24 +0000

    switch to pg database

--- Gemfile
+++ Gemfile
@@ -1,11 +1,14 @@
 source 'https://rubygems.org'
 
-
 gem 'rails', '4.1.5'
 
 gem 'rails-api'
 
 gem 'puma'
+
+gem 'mina'
+
+gem 'pg'
 
 gem 'spring', :group => :development
 
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150105170751) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "apns_tokens", force: true do |t|
     t.integer  "user_id"
     t.string   "token",      null: false
