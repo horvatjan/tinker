657e297e41d68f1848ceb216ef7d0f597cc11d49
jqn <jan.horvat@gmail.com>
2015-03-09 17:19:02 +0000

    invite: if app user, automatically added as friend

--- app/controllers/api/v1/friends_controller.rb
+++ app/controllers/api/v1/friends_controller.rb
@@ -37,11 +37,16 @@ module Api
         user = User.where(authentication_token: request.headers[:token])
 
         return error_response("Invitee's email is requred", 102) unless params[:email].present?
-        #return error_response("User has already been invited", 103) if Invite.where(invitee: params[:email]).present?
+        return error_response("You can not invite yourself", 103) if user.first.email == params[:email]
 
-        Invite.create(user_id: user.first.id, invitee: params[:email])
-
-        FriendMailer.send_invite(user, params[:email])
+        friend = User.where(email: params[:email])
+        if friend.present?
+          return error_response('You are already friends with this user', 104) unless Friend.where(user_id: user.first.id, friend_id: friend.first.id).blank?
+          new_friend(user, friend.first.id)
+        else
+          Invite.create(user_id: user.first.id, invitee: params[:email])
+          FriendMailer.send_invite(user, params[:email])
+        end
       end
 
       def create
@@ -54,19 +59,8 @@ module Api
         return error_response('Can not befriend yourself', 105) if user.first.id == params[:friend_id].to_i
         return error_response('You are already friends with this user', 106) unless Friend.where(user_id: user.first.id, friend_id: params[:friend_id]).blank?
 
-        friend = Friend.create(user_id: user.first.id, friend_id: params[:friend_id])
-
-
-
-        color = get_color(params[:friend_id])
-        text = "#{user.first.name} just added you."
-        Tink.create(user_id: user.first.id, recipient_id: params[:friend_id], read: 0, color: color, text: text)
-
-        ApnsToken.where(user_id: params[:friend_id]).each do |t|
-          send_push_notification(t.token, params[:tink][:recipient_id], text)
-        end
-
-        success_response({user_id: friend[:user_id], friend_id: friend[:friend_id], text: text})
+        new_friend(user, params[:friend_id])
+        success_response({user_id: user.first.id, friend_id: params[:friend_id]})
       end
 
       def destroy
@@ -80,6 +74,18 @@ module Api
 
         Friend.where(user_id: user.first.id).where(friend_id: params[:id]).delete_all
       end
+
+      def new_friend(user, friend_id)
+        Friend.create(user_id: user.first.id, friend_id: friend_id)
+        color = get_color(friend_id)
+        text = "#{user.first.name} just added you."
+        Tink.create(user_id: user.first.id, recipient_id: friend_id, read: 0, color: color, text: text)
+
+        ApnsToken.where(user_id: friend_id).each do |t|
+          send_push_notification(t.token, friend_id, text)
+        end
+      end
+
     end
   end
 end
