8bd2ab701117c1568f5e771928b2c0ace8a71734
jqn <jan.horvat@gmail.com>
2015-04-28 12:40:12 +0000

    confirmation page design and frontpage design

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < BaseController
 
     if User.where(email_confirmation_code: params[:id]).present?
       User.where(email_confirmation_code: params[:id]).update_all(active: 1)
-      @text = 'Profile completed! When you sign in we will interactively show you how to add and tink your friends.'
+      @text = '<b>Profile completed!</b><br><br> When you <a href="#">sign in</a> we will interactively show you how to add and tink your friends.'
     else
       @text = 'Problems with account activation. Please, try again.'
     end
@@ -19,10 +19,17 @@ class PagesController < BaseController
     return
   end
 
+  def contact
+    if params[:subject] == 'Android'
+      @value = 'I want an Android version of Tinkchat'
+    else
+      @value = params[:subject]
+    end
+  end
+
   private
     def message_params
       final_params = params.permit(:subject, :email, :message)
       final_params
     end
-
 end
