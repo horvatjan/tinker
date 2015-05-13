ac04ee341f25399c73866927191a476cdf31be02
jqn <jan.horvat@gmail.com>
2015-05-13 18:28:40 +0000

    small fixes (google analytics, design changes etc..)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < BaseController
 
     if User.where(email_confirmation_code: params[:id]).present?
       User.where(email_confirmation_code: params[:id]).update_all(active: 1)
-      @text = '<b>Profile completed!</b><br><br> When you <a href="#">sign in</a> we will interactively show you how to add and tink your friends.'
+      @text = '<b>Profile completed!</b><br><br> When you <a href="#">sign in</a> we will show you how to add and tink your friends.'
     else
       @text = 'Problems with account activation. Please, try again.'
     end
