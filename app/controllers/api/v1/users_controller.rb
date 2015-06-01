2d4fea6ef63ba9d7df4a6ebdba01f0e183334a04
jqn <jan.horvat@gmail.com>
2015-06-01 17:21:55 +0000

    user_id returned at find users endpoinnt

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,8 +7,8 @@ module Api
       def index
         auth_user and return
 
-        by_name_username = User.select("id, name, email, username").where("name || username ILIKE ?", "%#{params[:keyword]}%").limit(50)
-        by_email = User.select("id, name, email, username").where("email_visibility = 1 AND email ILIKE ?", "%#{params[:keyword]}%").limit(50)
+        by_name_username = User.select("id AS user_id, name, email, username").where("name || username ILIKE ?", "%#{params[:keyword]}%").limit(50)
+        by_email = User.select("id AS user_id, name, email, username").where("email_visibility = 1 AND email ILIKE ?", "%#{params[:keyword]}%").limit(50)
 
         response = []
         by_name_username.each do |user|
