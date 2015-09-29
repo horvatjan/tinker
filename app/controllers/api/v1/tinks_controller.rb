23bd995c3e2634b65eacefabc531f38f5cdc16bb
jqn <jan.horvat@gmail.com>
2015-09-29 17:53:45 +0000

    hashtags added to tinks

--- app/controllers/api/v1/tinks_controller.rb
+++ app/controllers/api/v1/tinks_controller.rb
@@ -29,7 +29,7 @@ module Api
         return error_response('Recipient has been banned', 105) unless Ban.where(user_id: user.first.id, banned_id: params[:tink][:recipient_id]).empty?
 
         color = get_color(params[:tink][:recipient_id])
-        Tink.create(user_id: user.first.id, recipient_id: params[:tink][:recipient_id], read: 0, color: color, text: "#{user.first.name} is thinking of you.")
+        Tink.create(user_id: user.first.id, recipient_id: params[:tink][:recipient_id], read: 0, color: color, text: "#{user.first.name} is thinking of you. #{(!params[:tink][:hashtags].empty?}")
 
         ApnsToken.where(user_id: params[:tink][:recipient_id]).each do |t|
           send_push_notification(t.token, params[:tink][:recipient_id], "Someone is thinking of you.")
